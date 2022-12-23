8ef9aefea98597293be59955acae14e7aa2afcda
Savenok <Romasav2017@gmail.com>
2022-12-23 15:27:41 +0300

    grade

---
+++ grade.py
@@ -0,0 +1,19 @@
+
+
+
+def write(sc ,predmet , gr):
+    with open('ggrade.txt', 'a' , encoding="utf-8") as f:
+        f.write(f"{sc} {predmet} {gr}\n")
+
+
+
+grades = {}
+def read(sc):
+    with open('ggrade.txt', 'r' , encoding="utf-8") as f:
+        data = str(f.read()).splitlines()
+        for line in data:
+            f1 = line.split()
+            grades[f'{f1[0]}'] = [f1[2]]
+        if sc in grades:
+            return grades[sc]
+
